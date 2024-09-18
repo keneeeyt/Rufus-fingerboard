0fe392b8f6c87fb7513336398cf539de73c1e947
Kenneth Louie Ermila Cervantes <111757975+keneeeyt@users.noreply.github.com>
2024-09-18 12:33:59 +0800

    Added whitelist email when update banner image

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -38,7 +38,7 @@ export const ourFileRouter = {
      const user = await getUser();
  
       // If you throw, the user will not be able to upload
-      if (!user || user.email !== `${process.env.VALID_EMAIL_ADMIN}`) throw new UploadThingError("Unauthorized");
+      if (!user || (user.email !== `${process.env.VALID_EMAIL_ADMIN}` && user.email !== 'rufusfingerboards@gmail.com')) throw new UploadThingError("Unauthorized");
  
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: user.id };
